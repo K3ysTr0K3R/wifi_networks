b28729683f32436f7842fe992e8747cf57a99a18
k3ystr0k3r <70909693+K3ysTr0K3R@users.noreply.github.com>
2023-01-22 22:20:54 -0700

    Update wifi_networks.py

--- wifi_networks.py
+++ wifi_networks.py
@@ -1,16 +1,32 @@
 #!/bin/python3
 
 import subprocess
+import wifi
 import re
 
+wifi_networks = wifi.Cell.all('wlan0')
 results = subprocess.run(['iwlist', 'wlan0', 'scan'], capture_output=True)
 networks = re.findall(r'Address: [A-F0-9:]{17}', results.stdout.decode())
 channels = re.findall(r'Channel:[^\d]*(\d+)', results.stdout.decode())
-mac = networks[0][9:]
-for line in results.stdout.decode().split("\n"):
-    match = re.search(r'ESSID:"(.*)"', line)
-    if match:
-        ssid = match.group(1)
-        print("SSID:", ssid)
-        print("MAC:", mac)
-        print("CHANNEL:", channels[0])
+
+try:
+    mac = networks[0][9:]
+    lines = results.stdout.decode().split("\n")
+    for line in lines:
+        match = re.search(r'ESSID:"(.*)"', line)
+        if match:
+            ssid = match.group(1)
+            print("")
+            if not ssid:
+                print("SSID: None")
+            else:
+                print("SSID:", ssid)
+            print("MAC:", mac)
+            print("CHANNEL:", channels[0])
+            for network in wifi_networks:
+                if network.address == mac:
+                    print("PASSWORD:", network.encrypted)
+                    break
+            print("")
+except IndexError:
+    print("[!] Couldnt capture WIFI networks")
